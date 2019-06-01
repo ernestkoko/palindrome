8089f8c2a75cc3107bcabfe6e6bc29081b8faa17
ernestkoko <ernesteferetin@yahoo.com>
2019-06-01 08:07:20 -0400

    Initialize repository

---
+++ index.js
@@ -0,0 +1,34 @@
+// Reverses a string.
+function reverse(string) {
+  return Array.from(string).reverse().join("");
+}
+
+function Phrase(content) {
+  this.content = content;
+
+  this.processor = function(string) {
+    // FILL IN
+
+    return Array.from(this).toLowerCase();
+  }
+
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === reverse(this.processedContent());
+  }
+}
+
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+TranslatedPhrase.prototype = new Phrase();
